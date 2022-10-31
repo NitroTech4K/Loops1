1e4447886d91588a1eaf0fb9951124bfdd0659d0
NitroTech 4K <crtraer@gmail.com>
2022-10-31 11:26:49 -0300

    After explaination, and added some explanation in my own words.

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Loops1
@@ -10,6 +11,44 @@ namespace Loops1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Loops!");
+            Console.WriteLine();
+
+            int start = 0;
+            int end = 4;
+
+            int[] ammo;
+            ammo = new int[5];
+            ammo[0] = 6;
+            ammo[1] = 2;
+            ammo[2] = 50;
+            ammo[3] = 25;
+            ammo[4] = 10;
+
+            int i = start;
+
+            //The "Count" variable is renamed to "i" so it's easier to put in the command.
+            //Count = i
+            //Supposed to be industry standard.
+
+            //Instead of "int count = start", put "int i = start". 
+
+            //The meaning of I = INDEX!!! Index is like the little number in the page of a book. So here we're referencing the ammo type like a page in a book". 
+
+            
+
+
+            while(i <= end)
+            {
+                Console.WriteLine("Weapon #" + i + "; " + ammo[i]);
+                
+                i = i + 1;
+            }
+            
+
+
+
+            Console.ReadKey(true);
         }
     }
 }
